0d87bbc2d10d8d510ce0fd5782ed995affe15f99
sadam <sadamccr@gmail.com>
2022-12-31 02:31:17 +0500

    home file  added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-    <div className="content">
-      <h1>App Component</h1>
-    </div>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
