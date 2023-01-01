f6aae1bf5c8abe430833be84b4d3aa9b6e567b98
sadam <sadamccr@gmail.com>
2023-01-01 13:09:08 +0500

    useState done

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
+    const handleClick = () => {
+        setName('khan')
+    }
+
+    let [name, setName] = useState('sadam')
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
+            <button onClick={() => handleClick()}>Click Me</button>
         </div>
     );
 }
- 
+
 export default Home;
