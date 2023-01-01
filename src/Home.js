ec49445071c2d2a44c92834e130fb955ec3c95b5
sadam <sadamccr@gmail.com>
2023-01-01 13:25:19 +0500

    blogs list added

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
 
 const Home = () => {
-    const handleClick = () => {
-        setName('khan')
-    }
 
-    let [name, setName] = useState('sadam')
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum', author: 'sadam', id: 1 },
+        { title: 'Welcome party', body: 'lorem ipsum', author: 'hamza', id: 2 },
+        { title: 'Web dev tips', body: 'lorem ipsum', author: 'asad', id: 3 },
+    ])
+    
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name}</p>
-            <button onClick={() => handleClick()}>Click Me</button>
+            {
+                blogs.map((blog)=>(
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>{blog.author}</p>
+                    </div>
+                ))
+            }
         </div>
     );
 }
