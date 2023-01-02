7eb47d6cfbb559d1b0a1aaba3dc51423eac2d184
sadam <sadamccr@gmail.com>
2023-01-02 13:28:06 +0500

    json file added

--- src/BlogList.js
+++ src/BlogList.js
@@ -7,7 +7,7 @@ const BlogList = ({ blogs, title, handleDelete }) => {
                     <div className="blog-preview" key={blog.id}>
                         <h2>{blog.title}</h2>
                         <p>written by {blog.author}</p>
-                        <button onClick={()=>handleDelete(blog.id)}>Delete</button>
+                        <button onClick={() => handleDelete(blog.id)}>Delete</button>
                     </div>
                 ))
             }
