2fc85d2a38f541ca09ce901aeba3728d057bcc61
sadam <sadamccr@gmail.com>
2023-01-03 23:39:37 +0500

    fetch errors done

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,35 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
     const [isPending, setPending] = useState(true)
+    const [error, setError] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlog = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlog)
-    }
+    // const handleDelete = (id) => {
+    //     const newBlog = blogs.filter(blog => blog.id !== id)
+    //     setBlogs(newBlog)
+    // }
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-            .then(res => { return res.json(); })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Couldnt find data..')
+                }
+                return res.json();
+            })
             .then(data => {
                 setBlogs(data);
                 setPending(false);
+                setError(null)
+            })
+            .catch((error) => {
+                isPending(false)
+                setError(error.message)
             })
     }, [])
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title={'All Blogs'} />}
         </div>
