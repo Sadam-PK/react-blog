104d73fe220c6a3cea6210294b39db6007e0d874
sadam <sadamccr@gmail.com>
2023-01-03 23:57:21 +0500

    updated

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,6 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setPending] = useState(true)
-    const [error, setError] = useState(null);
-
     // const handleDelete = (id) => {
     //     const newBlog = blogs.filter(blog => blog.id !== id)
     //     setBlogs(newBlog)
