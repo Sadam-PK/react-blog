d6c83d2c7e57c76ffa0a76e2b82b6b0987e6142e
sadam <sadamccr@gmail.com>
2023-01-06 01:29:11 +0500

    input fields added

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,45 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('sadam');
+
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
+            <form action="">
+                <label htmlFor="">Blog Title</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    placeholder='Title'
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <textarea
+                    required
+                    placeholder='Description'
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                >
+
+                </textarea>
+                <label htmlFor="">Blog Author:</label>
+                <select name="" id=""
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="sadam">sadam</option>
+                    <option value="hamza">hamza</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
     );
 }
- 
+
 export default Create;
